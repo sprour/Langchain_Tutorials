cf69720cb466d2f0b56dc62f51d9765d87595c6a
Sergei Prokhorov <sergei@backbase.com>
2024-05-26 00:21:46 +0300

    Add Remote runnuble chain for api

--- SimpleApp/app.py
+++ SimpleApp/app.py
@@ -6,6 +6,7 @@ from langchain_core.prompt_values import ChatPromptValue
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_vertexai import ChatVertexAI
 from langchain_openai import ChatOpenAI
+from langserve import RemoteRunnable
 
 load_dotenv()
 
@@ -33,8 +34,13 @@ prompt_template = ChatPromptTemplate.from_messages(
 
 # ChatPromptValue(messages=[SystemMessage(content='Translate the following into italian:'), HumanMessage(content='hi')])
 
+# We can easily create the chain using the | operator.
+# The | operator is used in LangChain to combine the elements together
 chain = prompt_template | model | parser
 
-result = chain.invoke({"language": "italian", "text": "hi"})
+# result = chain.invoke({"language": "italian", "text": "hi"})
+
+remote_chain = RemoteRunnable("http://localhost:8000/chain/")
+result = remote_chain.invoke({"language": "italian", "text": "hi"})
 
 print(result)
